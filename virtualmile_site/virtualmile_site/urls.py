61b214c538cab3deb78b36bbaaf75886b5940a78
chris <tiefenau@cs.uni-bonn.de>
2020-03-17 10:32:59 +0100

    first view

--- virtualmile_site/virtualmile_site/urls.py
+++ virtualmile_site/virtualmile_site/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+
 urlpatterns = [
+    path('mileservice/', include('mileservice.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
